4b71c529129cc7109d3bb60d7cec6d4577fabc72
Charles <GUITARPLRC@YAHOO.COM>
2016-03-31 15:07:10 -0500

    fixed issue with start, pause, set

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ var pause = document.getElementById("pause"); // pause button
 var setTime = document.getElementById("setTime"); // setTime button
 var timer = document.getElementById("timer"); // html timer display
 var minutes = document.getElementById("minutes"); // minutes user input
-var seconds = document.getElementById("seconds"); // seconds user input
 var interval; // interval for timer
 var minTime;
 var secTime;
 
 pause.disabled = true;
+
 start.addEventListener("click", startTimer, false);
 pause.addEventListener("click", pauseTimer, false);
 setTime.addEventListener("click", setTimeTimer, false);
 
 minutes.value = 20; // default time set
-seconds.value = 0; // default time set
+
 // get user input and set time for timer
 minTime = Number(minutes.value);
-secTime = Number(seconds.value);
+secTime = 0;
 
 if (secTime < 10) {
 	
@@ -30,6 +30,7 @@ timer.textContent = minTime + " Minutes " + secTime + " Second(s) Left";
 
 function startTimer() {
 	
+	setTime.disabled = true;
 	start.disabled = true;
 	pause.disabled = false;
 	
@@ -42,7 +43,7 @@ function startTimer() {
 			
 		}	else {
 			
-			if (secTime <= 0 && minTime > 0) {
+			if (secTime < 0 && minTime > 0) {
 				// subtract 1 minute when seconds reach 0
 				minTime -= 1;
 				secTime = 59;
@@ -67,6 +68,7 @@ function startTimer() {
 
 function pauseTimer() {
 	
+	setTime.disabled = false;
 	start.disabled = false;
 	pause.disabled = true;
 	
@@ -80,14 +82,14 @@ function setTimeTimer() {
 	pause.disabled = true;
 	
 	minTime = Number(minutes.value);
-	secTime = Number(seconds.value);
+	secTime = 0;
 	
 	if (secTime < 10) {
-		
-		// add 0 in front of seconds when below 10
-		secTime = "0" + secTime;
-		
-	}
+				
+				// add 0 in front of seconds when below 10
+				secTime = "0" + secTime;
+				
+			}
 	
 	timer.textContent = minTime + " Minutes " + secTime + " Second(s) Left";
 	
